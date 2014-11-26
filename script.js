6399f75a8db33de1fb8b4e9694767f94bb6137e8
JoeyHayden <joey.s.hayden@gmail.com>
2014-11-25 21:29:49 -0800

    re-submitted changes due to them not being saved on sublime

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+$(document).ready(function() {
+var numbers =[4,2,5,1,0,3];
+console.log(numbers);
+
+function bubbleSort(numbers)
+{
+    var swapped;
+    do {
+        swapped = false;
+        for (var i=0; i < numbers.length-1; i++) {
+            if (a[i] > a[i+1]) {
+                var temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                swapped = true; 
+            }
+        }
+    } while (swapped); 
+    console.log(swapped);
+}
+ 
+console.log(numbers);
+
+
+});
