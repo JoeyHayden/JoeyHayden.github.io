8d9c0be441d8ce1b70b854a48a264095fbfef2a5
JoeyHayden <joey.s.hayden@gmail.com>
2014-12-03 23:26:41 -0800

    updated the readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 JoeyHayden.github.io
 ====================
+This project was built in partnership with darren cools http://darrencools.github.io/. we used a bubble sort to organize a scrambled set of colors in their order of wavelength. the click shuffle inspiration came from http://bost.ocks.org/mike/shuffle/ 
+
+you can find my work at https://github.com/JoeyHayden
+
+thanks for visiting!
--- script.js
+++ script.js
@@ -1,39 +1,43 @@
+
+
+
 $(document).ready(function() {
-
-//global variables 
-var numbers = [0,1,2,3,4,5,];
-
-
-// On click function to shuffle
-//http://bost.ocks.org/mike/shuffle/
-$("#btn2").click(function() {
-
-var m = numbers.length, t, i;
-
-// While there remain elements to shuffle…
-  while (m) {
-
-// Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-
-// And swap it with the current element.
-    t = numbers[m];
-    numbers[m] = numbers[i];
-    $('#' + i).appendTo('#colorList');
-    numbers[i] = t;
-  }
-});
-
-//On click sort function
-$("#btn1").click(function() {
-
-  var tmp;
-    for (var i = 0; i < numbers.length; i += 1) {
-      for (var j = i; j > 0; j -= 1) {
-        if (numbers[j] < numbers[j - 1]) {
-          tmp = numbers[j];
-          numbers[j] = numbers[j - 1];
-          numbers[j - 1] = tmp;
+ 
+  //global variables 
+  var circles = [0, 1, 2, 3, 4, 5];
+ 
+  // On click function to shuffle
+  // http://bost.ocks.org/mike/shuffle/
+  $("#btn2").click(function() {
+ 
+    var rand, tmp, i; 
+ 
+    for (i = circles.length; i > 0; i--) {
+ 
+      // select random index
+      rand = Math.floor(Math.random() * i);
+ 
+      // swap current with random
+      tmp = circles[rand];
+      circles[rand] = circles[i];
+      circles[i] = tmp;
+ 
+      // move element
+      $('#' + rand).appendTo('#colorList');
+    }
+ 
+  });
+ 
+  //On click sort function
+  $("#btn1").click(function() {
+ 
+    var tmp, i, j;
+    for (i = 0; i < circles.length; i++) {
+      for (j = i; j > 0; j--) {
+        if (circles[j] < circles[j - 1]) {
+          tmp = circles[j];
+          circles[j] = circles[j - 1];
+          circles[j - 1] = tmp;
         }
       }
       $('#' + i).appendTo('#colorList');
